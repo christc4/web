e61570d2b90b7bd8f1ee5f8a94f3f591d52edbaa
Charlie Root <root@avsbq.org>
2024-09-18 04:33:30 +0000

    New pages written

--- sites/handbook.avsbq.org/s.js
+++ sites/handbook.avsbq.org/s.js
@@ -12,26 +12,26 @@ function filterResults(query, siteIndex) {
 
 // Function to display search results
 function displayResults(results) {
-    const searchResults = document.getElementById('searchResults');
-    searchResults.innerHTML = '';
+    const sR = document.getElementById('sR');
+    sR.innerHTML = '';
 
     // Slice the results to show a maximum of 5 items
     results.slice(0, 4).forEach(result => {
         const resultItem = document.createElement('div');
         resultItem.className = 'resultItem';
         resultItem.innerHTML = `<a href="/${result}">${result}</a>`;
-        searchResults.appendChild(resultItem);
+        sR.appendChild(resultItem);
     });
 }
 
 // Event listener for search input
-document.getElementById('searchBar').addEventListener('input', async function() {
-    const query = this.value;
-    const searchResults = document.getElementById('searchResults');
-    
-    // Clear search results if input is empty
-    if (query === '') {
-        searchResults.innerHTML = '';
+document.getElementById('sB').addEventListener('input', async function() {
+const query = this.value;
+const sR = document.getElementById('sR');
+
+// Clear search results if input is empty
+if (query === '') {
+sR.innerHTML = '';
         return;
     }
 
